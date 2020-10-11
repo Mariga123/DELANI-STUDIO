7a8618122c5644467d8ff11537874a48007041ab
Mariga123 <johnmariga8@gmail.com>
2020-10-11 12:42:01 +0300

    make the form responsive to alert the user using javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,17 @@ $(".image").click(function () {
     $(this).children(".toggle").slideToggle();
     $(this).children(".show").toggle();
 });
+$("form#formInput").submit(function (event) {
+    let name = $("input#name").val();
+    let email = $("input#email").val();
+    let subject = $("textarea#subject").val();
+
+    if ($("input#name").val() && $("input#email").val()) {
+        alert(name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+        alert("Please enter your name and email!");
+    }
+
+    event.preventDefault();
+})
