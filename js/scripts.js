f9bbcb6fca51d7ba8ba7cf084d50bf7c078b5105
Mariga123 <johnmariga8@gmail.com>
2020-10-11 13:46:06 +0300

    link mailchimp to the form after recieving feedback

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,19 @@ $(".image").click(function () {
     $(this).children(".toggle").slideToggle();
     $(this).children(".show").toggle();
 });
-$("form#formInput").submit(function (event) {
-    let name = $("input#name").val();
-    let email = $("input#email").val();
-    let subject = $("textarea#subject").val();
+$(document).ready(function (event) {
+    $("form#formInput").submit(function() {
+        let name = $("input#name").val();
+        let email = $("input#email").val();
+        let subject = $("textarea#subject").val();
 
-    if ($("input#name").val() && $("input#email").val()) {
-        alert(name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {
-        alert("Please enter your name and email!");
-    }
+        if ($("input#name").val() && $("input#email").val()) {
+            alert(name + ", we have received your message. Thank you for reaching out to us.");
+        }
+        else {
+            alert("Please enter your name and email!");
+        }
 
-    event.preventDefault();
-})
+        event.preventDefault();
+    })
+    });
